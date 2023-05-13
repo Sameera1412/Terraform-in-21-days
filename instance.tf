c33a194f7ba16c3204adcac236f5dffa855df617
Sameera <iamsameerashaik@gmail.com>
2023-05-12 23:30:06 -0300

    Create EC2 instance

---
+++ instance.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "public" {
+  ami                         = "ami-08333bccc35d71140"
+  associate_public_ip_address = true
+  instance_type               = "t3.micro"
+  key_name                    = "Terraform_demo"
+  vpc_security_group_ids      = []
+  subnet_id                   = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-public"
+  description = "Allow inbound traffic"
+  vpc_id        = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["142.188.74.67/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+
+  }
+}
