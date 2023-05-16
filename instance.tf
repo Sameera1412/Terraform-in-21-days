a269685447309701cd3d058d5c3940cf5a0dd80b
Sameera <iamsameerashaik@gmail.com>
2023-05-16 20:18:35 -0300

    Opening port 80 using user-data

--- instance.tf
+++ instance.tf
@@ -19,6 +19,8 @@ resource "aws_instance" "public" {
   key_name                    = "Terraform"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = aws_subnet.public[0].id
+  user_data                   = file("user-data.sh")
+
 
   tags = {
     Name = "${var.env_code}-public"
